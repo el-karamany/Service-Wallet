510e0c99b83612869482ec3559d824ba655d7592
Omar El-Karamany <75332083+el-karamany@users.noreply.github.com>
2026-03-23 22:06:54 +0200

    Delete service_wallet directory

--- service_wallet/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    
-    
-    is_prepaid_pack = fields.Boolean(string='Prepaid Pack', default=False, help="Is this product a support pack?")
-    pack_hours = fields.Float(string='Pack Hours', help="How many hours does this pack contain?")
